52359e48961dfd8a2f3e17bec43e2afd326b2044
YoungbinKim <colock123@gmail.com>
2022-04-22 01:03:55 +0900

    feat:core추가

--- students/youngbin/postings/models.py
+++ students/youngbin/postings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Post(models.Model):
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        db_table = posts
+    
